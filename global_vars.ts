cbbacfafcec85bfa013bae4d5a6f66edeb027a8c
Toriong <gtorion97@gmail.com>
2024-06-11 14:44:18 -0500

    adding the 'review and feedback' section

--- global_vars.ts
+++ global_vars.ts
@@ -4,13 +4,19 @@ const IMGS = (() => {
     const PERMISSIONS_ICON = "https://effingo.s3.us-east-2.amazonaws.com/permissions-icon.png";
     const LOGO_200_X_200 = "https://effingo.s3.us-east-2.amazonaws.com/logo-nobackground-200.png";
     const LOGO_500_X_500 = "https://effingo.s3.us-east-2.amazonaws.com/logo-original-500.png";
-
+    const SUBSCRIPTION_ICON = "https://effingo.s3.us-east-2.amazonaws.com/subscription-icon.png";
+    const FIVE_STAR_ICON = "https://effingo.s3.us-east-2.amazonaws.com/five-stars-icon.png";
+    const FEEDBACK_ICON = "https://effingo.s3.us-east-2.amazonaws.com/feedback-icon.png";
+    
     return { 
         COPY_ICON,
         PERMISSIONS_ICON,
         SHARE_ICON,
         LOGO_200_X_200,
-        LOGO_500_X_500
+        LOGO_500_X_500,
+        SUBSCRIPTION_ICON,
+        FIVE_STAR_ICON,
+        FEEDBACK_ICON
     }
 })();
 const CARDSERVICE_VARS = (() => {
