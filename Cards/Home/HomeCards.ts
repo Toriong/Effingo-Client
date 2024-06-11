754a1e4f25de04d7e7b6ac69754028d48b754fe6
Toriong <gtorion97@gmail.com>
2024-06-11 17:13:28 -0500

    creating a handler when the user clicks on the copy folder card on the home page

---
+++ Cards/Home/HomeCards.ts
@@ -0,0 +1,112 @@
+const HomeCards = (() => {
+	const { createHeader } = CardServices;
+
+	/**
+	 *
+	 * @remarks
+	 * Creates all of the cards that is displayed on the main menu of the home page.
+	 *
+	 * @returns Retruns each card of the home page.
+	 *
+	 * @beta
+	 */
+	function createHomePgCards() {
+		const { SQUARE } = CARDSERVICE_VARS;
+		const mainMenuHeader = CardService.newCardBuilder()
+			.setHeader(CardService.newCardHeader().setTitle("Tools"))
+			.build();
+		const reviewAndFeedbackHeader = createHeader(
+			"Feedback & Review",
+			"",
+			"",
+			SQUARE,
+			"",
+		);
+		const reviewAndFeedbackTitleCard = CardService.newCardBuilder()
+			.setHeader(reviewAndFeedbackHeader)
+			.build();
+		const feedbackHeader = createHeader(
+			"Feedback",
+			IMGS.FEEDBACK_ICON,
+			"subscription_icon",
+			SQUARE,
+			"Give us feedback or contact us. We want to improve!",
+		);
+		const reviewHeader = createHeader(
+			"Review",
+			IMGS.FIVE_STAR_ICON,
+			"review_icon",
+			SQUARE,
+			"Give us a rating 👍!",
+		);
+		const reviewCard = CardService.newCardBuilder()
+			.setHeader(reviewHeader)
+			.build();
+		const feedbackCard = CardService.newCardBuilder()
+			.setHeader(feedbackHeader)
+			.build();
+		const titleHeaderForSubscriptionSec = createHeader(
+			"Subscription",
+			"",
+			"",
+			SQUARE,
+			"",
+		);
+		const titleCardForSubscriptionSec = CardService.newCardBuilder()
+			.setHeader(titleHeaderForSubscriptionSec)
+			.build();
+		const subscriptionSecHeader = createHeader(
+			"Your Subscription",
+			IMGS.SUBSCRIPTION_ICON,
+			"SUBSCRIPTION_ICON",
+			SQUARE,
+			"View or update your subscription.",
+		);
+		const subscriptionSecCard = CardService.newCardBuilder()
+			.setHeader(subscriptionSecHeader)
+			.build();
+		const folderCopyCardHeader = createHeader(
+			"Folder Copy",
+			IMGS.COPY_ICON,
+			"folder_copy_icon",
+			SQUARE,
+			"Copy all or some items of a folder or just its structure.",
+		);
+		const folderCopyCard = CardService.newCardBuilder()
+			.setHeader(folderCopyCardHeader)
+			.build();
+		const shareCardHeader = createHeader(
+			"Share",
+			IMGS.SHARE_ICON,
+			"share_icon",
+			SQUARE,
+			"Share multiple folders/files regardless if they are nested.",
+		);
+		const shareCard = CardService.newCardBuilder()
+			.setHeader(shareCardHeader)
+			.build();
+		const permissionsCardHeader = createHeader(
+			"Permissions",
+			IMGS.PERMISSIONS_ICON,
+			"permissions_icon",
+			SQUARE,
+			"Revoke access for multiple users. Search permissions by email.",
+		);
+		const permissionsCard = CardService.newCardBuilder()
+			.setHeader(permissionsCardHeader)
+			.build();
+		const tools = [mainMenuHeader, folderCopyCard, shareCard, permissionsCard];
+		const subscriptionVals = [titleCardForSubscriptionSec, subscriptionSecCard];
+		const feedbackAndReview = [
+			reviewAndFeedbackTitleCard,
+			feedbackCard,
+			reviewCard,
+		];
+
+		return [...tools, ...subscriptionVals, ...feedbackAndReview];
+	}
+
+	return {
+		createHomePgCards,
+	};
+})();
---
+++ Cards/cardServices.ts
@@ -0,0 +1,25 @@
+const CardServices = (() => {
+	type TImageStyle =
+		| typeof CardService.ImageStyle.CIRCLE
+		| typeof CardService.ImageStyle.SQUARE;
+	function createHeader(
+		title: string,
+		imgUrl: string,
+		imgAlt: string,
+		imgStyle: TImageStyle = CardService.ImageStyle.SQUARE,
+		subTitle = "",
+	) {
+		const header = CardService.newCardHeader()
+			.setTitle(title)
+			.setSubtitle(subTitle)
+			.setImageUrl(imgUrl)
+			.setImageAltText(imgAlt)
+			.setImageStyle(imgStyle);
+
+		return header;
+	}
+
+	return {
+		createHeader,
+	};
+})();
