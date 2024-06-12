01f8d3356d2ff76af7dbdda1484e96b04a58d8f7
Toriong <gtorion97@gmail.com>
2024-06-12 18:48:14 -0500

    updating the appsscript json manifest

---
+++ global_fns.ts
@@ -0,0 +1,58 @@
+//FOR THE HOME CARD FUNCTIONS
+function handleCopyFolderStructureBtn() {}
+
+function handleFolderItemsCopy() {
+	const { SQUARE } = CARDSERVICE_VARS;
+	const { createHeader } = CardServices;
+	const folderCopyCardHeader = createHeader(
+		"Folder Copy",
+		IMGS.COPY_ICON,
+		"folder_copy_icon",
+		SQUARE,
+		"yo thereeee!.",
+	);
+	const card = CardService.newCardBuilder()
+		.setHeader(folderCopyCardHeader)
+		.build();
+	const x = CardService.newNavigation().pushCard(card);
+
+	//
+
+	return x;
+}
+
+function displayFolderCards() {
+	const { createHeader } = CardServices;
+
+	const copyFolderStructureHeader = createHeader(
+		"Structure copy",
+		IMGS.ICON_FOLDER_STRUCTURE,
+		"copy_folder_structure_icon",
+		CardService.ImageStyle.SQUARE,
+		"Copy only the folder's sub folders.",
+	);
+	const copyFolderStructureOptCard = CardService.newCardBuilder()
+		.setHeader(copyFolderStructureHeader)
+		.build();
+
+	const deepCopyHeader = createHeader(
+		"Deep Copy",
+		IMGS.ICON_COPY_FOLDER_OPT,
+		"deep_copy_icon",
+		CardService.ImageStyle.SQUARE,
+		"Copy folders and their contents.",
+	);
+	const deepCopyCard = CardService.newCardBuilder()
+		.setHeader(deepCopyHeader)
+		.build();
+
+	const navigation = CardService.newNavigation()
+		.pushCard(copyFolderStructureOptCard)
+		.pushCard(deepCopyCard);
+
+	const actionResponse = CardService.newActionResponseBuilder()
+		.setNavigation(navigation)
+		.build();
+
+	return actionResponse;
+}
