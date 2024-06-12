cac323bae626430e1c67c76cf6f697b9f7d6b0a7
Toriong <gtorion97@gmail.com>
2024-06-12 18:14:56 -0500

    handling gdrive item selection

--- Cards/Home/HomeCards.ts
+++ Cards/Home/HomeCards.ts
@@ -1,57 +1,3 @@
-function execute() {
-	const { SQUARE } = CARDSERVICE_VARS;
-	const { createHeader } = CardServices;
-	const folderCopyCardHeader = createHeader(
-		"Folder Copy",
-		IMGS.COPY_ICON,
-		"folder_copy_icon",
-		SQUARE,
-		"yo thereeee!.",
-	);
-	const card = CardService.newCardBuilder()
-		.setHeader(folderCopyCardHeader)
-		.build();
-	const x = CardService.newNavigation().pushCard(card);
-
-	return x;
-}
-
-function displayFolderCards() {
-	const { createHeader } = CardServices;
-
-	const copyFolderStructureHeader = createHeader(
-		"Structure copy",
-		IMGS.ICON_FOLDER_STRUCTURE,
-		"copy_folder_structure_icon",
-		CardService.ImageStyle.SQUARE,
-		"Copy only the folder's sub folders.",
-	);
-	const copyFolderStructureOptCard = CardService.newCardBuilder()
-		.setHeader(copyFolderStructureHeader)
-		.build();
-
-	const deepCopyHeader = createHeader(
-		"Deep Copy",
-		IMGS.ICON_COPY_FOLDER_OPT,
-		"deep_copy_icon",
-		CardService.ImageStyle.SQUARE,
-		"Copy folders and their contents.",
-	);
-	const deepCopyCard = CardService.newCardBuilder()
-		.setHeader(deepCopyHeader)
-		.build();
-
-	const navigation = CardService.newNavigation()
-		.pushCard(copyFolderStructureOptCard)
-		.pushCard(deepCopyCard);
-
-	const actionResponse = CardService.newActionResponseBuilder()
-		.setNavigation(navigation)
-		.build();
-
-	return actionResponse;
-}
-
 const HomeCards = (() => {
 	const { createHeader } = CardServices;
 
@@ -61,6 +7,27 @@ const HomeCards = (() => {
 		return CardService.newCardAction().setOnClickAction(
 			CardService.newAction().setFunctionName(fnName).setParameters(parameters),
 		);
+	}
+
+	function createHomeCardSection(
+		headerTxt: string,
+		btnTxt: string,
+		backgroundColor: string,
+		fnName: string,
+		fnParameters: TParameters = {},
+	) {
+		const cardAction = CardService.newAction()
+			.setFunctionName(fnName)
+			.setParameters(fnParameters);
+		const textButton = CardService.newTextButton()
+			.setText(btnTxt)
+			.setBackgroundColor(backgroundColor)
+			.setOnClickAction(cardAction);
+		const cardSection = CardService.newCardSection()
+			.setHeader(headerTxt)
+			.addWidget(textButton);
+
+		return cardSection;
 	}
 
 	function createHomePgCards() {
@@ -134,34 +101,17 @@ const HomeCards = (() => {
 			.setText("Copy items.")
 			.setBackgroundColor("#7AC4FB")
 			.setOnClickAction(cardAction);
-		const copyFolderStructureOpt = CardService.newTextButton()
-			.setText("Copy structures.")
-			.setBackgroundColor("#7AC4FB")
-			.setOnClickAction(cardAction);
-		const header = createHeader(
-			"Deep Copy",
-			IMGS.ICON_COPY_FOLDER_OPT,
-			"deep_copy_icon",
-			SQUARE,
-			"Copy all items.",
-		);
-		const txt = CardService.newTextParagraph().setText(
-			"Copy the content (the sub files/folders) of the selected folders.",
-		);
-		const copyFolderStructureTxt = CardService.newTextParagraph().setText(
-			"Copy only the sub folders of the parent folders (structure).",
-		);
 		const copyFolderItemsSection = CardService.newCardSection()
 			.setHeader(
 				"Copy the contents (the sub files/folders) of the selected folders.",
 			)
 			.addWidget(copyFolderContentOpt);
-		// RESPOND TO THE USER CLICKING ON A FOLDER
-		// GET ITS ID
-		// SEND A REQUEST TO COPY THAT FOLDER TO YOUR RUST SERVER
-		const copyFolderStructureSection = CardService.newCardSection()
-			.setHeader("Copy only the sub folders of the parent folders (structure).")
-			.addWidget(copyFolderStructureOpt);
+		const copyFolderStructureSection = createHomeCardSection(
+			"Copy only the sub folders of the selected parent folders (structure).",
+			"Copy structures.",
+			"#7AC4FB",
+			"handleCopyFolderStructureBtn",
+		);
 		const folderCopyCard = CardService.newCardBuilder()
 			.setHeader(folderCopyCardHeader)
 			.addSection(copyFolderItemsSection)
